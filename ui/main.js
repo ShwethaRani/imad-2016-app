ab33204662707e6055eee7d768f8149d1995436a
ShwethaRani <doshweth@gmail.com>
2016-09-26 21:29:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,9 @@ var element=document.GetElementById('main-text'
 );
 
 element.innerHTML='New value';
+
+//Move the image
+var img = document.getElementById('img');
+img.onclick=function (){
+   img.style.marginLeft = '100px'; 
+};
